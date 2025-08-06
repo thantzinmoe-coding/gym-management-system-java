ea09afd1bdf196cf55c5dab1e26986676f20b398
Thant-Zin-Moe <142769260+Thant-Zin-Moe@users.noreply.github.com>
2025-08-06 21:08:31 +0630

    manage-equipment

---
+++ src/main/java/org/_java_proj/gym_management_system/features/manageEquipment/dto/request/EquipmentCreateRequest.java
@@ -0,0 +1,4 @@
+package org._java_proj.gym_management_system.features.manageEquipment.dto.request;
+
+public class EquipmentCreateRequest {
+}
---
+++ src/main/java/org/_java_proj/gym_management_system/features/manageEquipment/service/impl/EquipmentServiceImpl.java
@@ -0,0 +1,4 @@
+package org._java_proj.gym_management_system.features.manageEquipment.service.impl;
+
+public class EquipmentServiceImpl {
+}
